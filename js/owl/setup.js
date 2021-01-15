fa7bb3db14bd222e4c455a80c1ace8f64993a0b3
Guilherme Sanches Simões <62891985+GuilhermeSanchesS@users.noreply.github.com>
2021-01-15 11:49:14 -0300

    Add initial 🖥

---
+++ js/owl/setup.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 500) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
